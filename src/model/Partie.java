3b186840d52b5b0ddfe30e56fea7259bcbd523d8
Louis <louis.navone7@etu.univ-lorraine.fr>
2018-04-11 15:08:57 +0200

    add methode existePartie()

--- src/model/Partie.java
+++ src/model/Partie.java
@@ -128,5 +128,14 @@ public class Partie extends Observable {
         this.notifyObservers();
     }
 
+    /**
+     * methode permettant de savoir si une partie
+     * peut être reprise ou non.
+     * @return
+     */
+    public boolean existePartie() {
+        // T0D0 : analyse des sauvegardes si existe
+        return true;
+    }
     
 }
