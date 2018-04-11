981212497aa582cb5bc45d6e2d6973015650143f
Louis <louis.navone7@etu.univ-lorraine.fr>
2018-04-11 15:55:45 +0200

    menu + controllerMenu

--- src/controller/ControllerMenu.java
+++ src/controller/ControllerMenu.java
@@ -1,7 +1,9 @@
 package controller;
 
 import model.Jeu;
+import view.VueMenu;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,6 +13,15 @@ public class ControllerMenu implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        JButton btn = (JButton) e.getSource();
+        switch(btn.getActionCommand()) {
+            case VueMenu.REPRENDRE_COMMANDE:
+                break;
+            case VueMenu.COMMENCER_COMMANDE:
+                break;
+            case VueMenu.QUITTER_COMMANDE:
+                System.exit(0);
+                break;
+        }
     }
 }
--- src/view/VueMenu.java
+++ src/view/VueMenu.java
@@ -12,6 +12,12 @@ import java.util.Observer;
 
 public class VueMenu extends Vue {
 
+    public final static int BUTTON_WIDTH = 300;
+    public final static int BUTTON_HEIGHT = 20;
+    public final static String REPRENDRE_COMMANDE = "reprendre";
+    public final static String COMMENCER_COMMANDE = "commencer";
+    public final static String QUITTER_COMMANDE = "quitter";
+
     private JButton reprendreButton = new JButton("Reprendre une partie");
     private JButton commencerButton = new JButton("Commencer une partie");
     private JButton quitterButton = new JButton("Quitter");
@@ -23,17 +29,39 @@ public class VueMenu extends Vue {
     public VueMenu() {
         JFrame f = new JFrame("Bataille Navale : menu");
         f.setPreferredSize(new Dimension(Vue.WIDTH, Vue.HEIGHT));
+        f.setLocationRelativeTo(null);
+
         this.controller = new ControllerMenu();
         this.panel = new JPanel();
         this.panel.setPreferredSize(new Dimension(Vue.WIDTH, Vue.HEIGHT));
-        this.panel.add(reprendreButton, BorderLayout.CENTER);
-        this.panel.add(commencerButton, BorderLayout.CENTER);
-        this.panel.add(quitterButton, BorderLayout.CENTER);
+        this.reprendreButton.setPreferredSize(new Dimension(VueMenu.BUTTON_WIDTH, VueMenu.BUTTON_HEIGHT));
+        this.commencerButton.setPreferredSize(new Dimension(VueMenu.BUTTON_WIDTH, VueMenu.BUTTON_HEIGHT));
+        this.quitterButton.setPreferredSize(new Dimension(VueMenu.BUTTON_WIDTH, VueMenu.BUTTON_HEIGHT));
+        this.reprendreButton.setActionCommand(VueMenu.REPRENDRE_COMMANDE);
+        this.commencerButton.setActionCommand(VueMenu.COMMENCER_COMMANDE);
+        this.quitterButton.setActionCommand(VueMenu.QUITTER_COMMANDE);
 
+        Box box = Box.createVerticalBox();
+        box.setPreferredSize(new Dimension(800,200));
+
+
+        this.panel.setLayout(new GridLayout(5,3, 20, 20));
         this.reprendreButton.setEnabled(false);
         reprendreButton.addActionListener((ActionListener) this.controller);
         commencerButton.addActionListener((ActionListener) this.controller);
         quitterButton.addActionListener((ActionListener) this.controller);
+
+        this.panel.add(new JPanel());
+        this.panel.add(new JPanel());
+        this.panel.add(new JPanel());
+        this.panel.add(new JPanel());
+        this.panel.add(reprendreButton);
+        this.panel.add(new JPanel());
+        this.panel.add(new JPanel());
+        this.panel.add(commencerButton).setLocation(2,1);
+        this.panel.add(new JPanel());
+        this.panel.add(new JPanel());
+        this.panel.add(quitterButton).setLocation(3,1);
 
         f.setContentPane(this.panel);
         f.setResizable(false);
